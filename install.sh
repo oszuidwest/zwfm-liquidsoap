de018853442fbfc808f035e37d296251252357a3
rmens <r.mens@me.com>
2022-08-25 20:48:54 +0200

    Fix typo

--- install.sh
+++ install.sh
@@ -5,10 +5,10 @@ sudo apt dist-upgrade -y
 sudo apt autoremove -y
 sudo apt install sudo opam -y
 
-#Install the ocaml package manager
-opam init --confirm-level yes
+# Install the ocaml package manager
+opam init -y
 
-# Instlal dependencies and packages
+# Install dependencies and packages
 opam install taglib mad lame vorbis cry flac samplerate ocurl liquidsoap liquidsoap-daemon fdkaac alsa --confirm-level unsafe-yes
 
 #Set path
