280b2f429946329ecf37900ea38c26edba6b62bf
Raymon Mens <r.mens@me.com>
2022-09-18 19:31:49 +0200

    Enable service

--- install.sh
+++ install.sh
@@ -13,6 +13,9 @@ wget https://github.com/savonet/liquidsoap/releases/download/v2.1.1/liquidsoap_2
 # Install deb package 
 sudo apt install /tmp/liq_2.2.1_amd64.deb --fix-broken
 
+# Install service
+wget https://raw.githubusercontent.com/oszuidwest/liquidsoap-ubuntu/Ubuntu-22.04-LTS/liquidsoap.service -O /etc/systemd/system/liquidsoap.service
+
 # Make dir for files
 sudo mkdir /etc/liquidsoap
 sudo chown -R liquidsoap:liquidsoap /etc/liquidsoap
