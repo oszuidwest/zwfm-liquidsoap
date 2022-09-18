189198021fef9929cd320354dee916bc8b0ed4d5
Raymon Mens <r.mens@me.com>
2022-09-18 18:12:36 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,11 @@ sudo apt upgrade -y
 sudo apt dist-upgrade -y
 sudo apt autoremove -y
 
-## WIP
+# Install FDKAAC and bindings
+sudo apt install fdkaac libfdkaac-ocaml libfdkaac-ocaml-dynlink -y
+
+# Get deb package
+wget https://github.com/savonet/liquidsoap/releases/download/v2.1.1/liquidsoap_2.1.1-ubuntu-jammy-1_amd64.deb -O /tmp/liq_2.2.1_amd64.deb
+
+# Install deb package 
+sudo apt install ./tmp/liq_2.2.1_amd64.deb --fix-broken
