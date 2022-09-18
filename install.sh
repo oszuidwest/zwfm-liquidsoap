ad2f69be970e2d58943641bdfc017bd4e7954d45
Raymon Mens <r.mens@me.com>
2022-09-18 18:08:12 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,13 +3,5 @@ sudo apt update -y
 sudo apt upgrade -y
 sudo apt dist-upgrade -y
 sudo apt autoremove -y
-sudo apt install sudo opam -y
 
-# Init the ocaml package manager
-opam init -y
-
-# Install dependencies and packages
-opam install taglib mad lame vorbis cry flac samplerate ocurl liquidsoap fdkaac alsa --confirm-level unsafe-yes
-
-# Set path
-eval $(opam env)
+## WIP
