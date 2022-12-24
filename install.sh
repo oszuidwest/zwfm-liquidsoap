973b335ca9b5330ea6e3bac01340ea5a04ed6f36
Raymon Mens <r.mens@me.com>
2022-12-24 15:09:02 +0100

    Update from main base (#5)

--- install.sh
+++ install.sh
@@ -18,23 +18,27 @@ if [ "$(cat /etc/debian_version)" != "bookworm/sid" ]; then
 fi
 
 # Update OS
-sudo apt update -y 
-sudo apt upgrade -y
-sudo apt dist-upgrade -y
-sudo apt autoremove -y
+sudo apt --quiet --quiet --yes update
+sudo apt --quiet --quiet --yes upgrade
+sudo apt --quiet --quiet --yes dist-upgrade
+sudo apt --quiet --quiet --yes autoremove
 
 # Install FDKAAC and bindings
 sudo apt install fdkaac libfdkaac-ocaml libfdkaac-ocaml-dynlink -y
 
 # Get deb package
-wget https://github.com/savonet/liquidsoap/releases/download/v2.1.2/liquidsoap_2.1.2-ubuntu-jammy-1_amd64.deb -O /tmp/liq_2.1.2_amd64.deb
+wget https://github.com/savonet/liquidsoap/releases/download/v2.1.3/liquidsoap_2.1.3-ubuntu-jammy-1_amd64.deb -O /tmp/liq_2.1.3_amd64.deb
 
 # Install deb package 
-sudo apt install /tmp/liq_2.1.2_amd64.deb --fix-broken
+sudo apt install /tmp/liq_2.1.3_amd64.deb --fix-broken --yes
 
-# Make dir for files
+# Make dirs for files
 sudo mkdir /etc/liquidsoap
-sudo chown -R liquidsoap:liquidsoap /etc/liquidsoap
+sudo mkdir /var/audio
+sudo chown -R liquidsoap:liquidsoap /etc/liquidsoap /var/audio
+
+# Download sample fallback file
+sudo wget https://upload.wikimedia.org/wikipedia/commons/6/66/Aaron_Dunn_-_Sonata_No_1_-_Movement_2.ogg -O /var/audio/fallback.ogg
 
 # Download radio.liq
 sudo wget https://raw.githubusercontent.com/oszuidwest/liquidsoap-ubuntu/main/radio.liq -O /etc/liquidsoap/radio.liq
