e08204755ad319d0946d4b44bb015622842092a9
Raymon Mens <r.mens@me.com>
2022-12-24 15:09:02 +0100

    Update from main base (#5)

--- icecast2.sh
+++ icecast2.sh
@@ -3,25 +3,25 @@
 set -e
 
 if [ "$(id -u)" != "0" ]; then
-	printf "You must be root to execute the script. Exiting."
-	exit 1
+    printf "You must be root to execute the script. Exiting."
+    exit 1
 fi
 
 if [ "$(uname -s)" != "Linux" ]; then
-	printf "This script does not support \"$(uname -s)\" Operating System. Exiting."
-	exit 1
+    printf "This script does not support \"$(uname -s)\" Operating System. Exiting."
+    exit 1
 fi
 
 if [ "$(cat /etc/debian_version)" != "bookworm/sid" ]; then
-	printf "This script only supports Ubuntu 22.04 LTS. Exiting."
-	exit 1
+    printf "This script only supports Ubuntu 22.04 LTS. Exiting."
+    exit 1
 fi
 
 clear
 printf "********************************\n"
 printf "ICECAST 2 INSTALLER\n"
 printf "********************************\n"
-printf "> Specify the host name (without http:// or www) please: "
+printf "> Specify the host name (for example: icecast.zuidwestfm.nl. Enter it without http:// or www) please: "
 read HOSTNAME
 printf "> Specify the source and relay password: "
 read SOURCEPASS
@@ -33,8 +33,20 @@ printf "> What's the admins e-mail (visible on admin pages and for let's encrypt
 read ADMINMAIL
 printf "> Do you want SSL (y/n)? "
 read SSL
-#Todo: ask user for port
-#Todo: if port is not 80 ssl is not possible
+printf "> Specify the port (default: 80): "
+read PORT
+
+# Assume port is 80 if no port was entered
+if [ -z "$PORT" ]; then
+    PORT=80
+    printf "You didn't specify a port. We assume port 80.\n"
+fi
+
+# If port is not 80 ssl is not possible
+if [ "$PORT" != "80" ]; then
+    SSL="n"
+    printf "Since the specified port is not 80, SSL is not possible. Disabling SSL.\n"
+fi
 
 # Set vars
 export DEBIAN_FRONTEND="noninteractive"
@@ -62,6 +74,8 @@ sudo apt --quiet --quiet --yes install icecast2 certbot
 # Post configuration
 sed -i 	-e "s|<location>[^<]*</location>|<location>$LOCATED</location>|" \
 	-e "s|<admin>[^<]*</admin>|<admin>$ADMINMAIL</admin>|" \
+	-e "s|<clients>[^<]*</clients>|<clients>250</clients>|" \
+	-e "s|<sources>[^<]*</sources>|<sources>5</sources>|" \
 	/etc/icecast2/icecast.xml 2>/dev/null 1>&2
 
 # Grant icecast access to ports < 1024
@@ -69,6 +83,17 @@ sudo setcap CAP_NET_BIND_SERVICE=+eip /usr/bin/icecast2
 
 # Apply post configuration
 service icecast2 restart
+
+# If port is 80 and SSL is enabled, nudge the user to run certbot
+if [ "$PORT" == "80" ] && [ "$SSL" == "y" ]; then
+    echo "You should edit icecast.xml to reflect the new port situation and get a certificate with certbot. I can't do that yet..."
+# If port is 80 and SSL is disabled, nudge the user to edit icecast.xml
+elif [ "$PORT" == "80" ] && [ "$SSL" == "n" ]; then
+    echo "You should edit icecast.xml to reflect the new port situation. I can't do that yet..."
+# If port is not 80 and SSL is not enabled, show a message
+else
+    echo "Icecast was installed. Happy streaming"
+fi
 
 ### SSL IS WIP
 ## This currently doesn't work because let's encrypt _requires_ validation over port 80 while icecast is on port 8000.
