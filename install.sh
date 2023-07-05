5add145d3fa7a0603549c21e1514dc6333bfe399
Raymon Mens <r.mens@me.com>
2023-07-05 21:11:52 +0200

    New beta

--- install.sh
+++ install.sh
@@ -44,7 +44,7 @@ if [ "$OS_SUPPORTED" = false ]; then
 fi
 
 # Set the liquidsoap package download URL based on OS version and architecture
-BASE_URL="https://github.com/savonet/liquidsoap/releases/download/rolling-release-v2.2.x/liquidsoap-03a5f07_2.2.0"
+BASE_URL="https://github.com/savonet/liquidsoap/releases/download/rolling-release-v2.2.x/liquidsoap-8bb250f_2.2.0"
 PACKAGE_URL="${BASE_URL}-${OS_ID}-${OS_VERSION}-1_${OS_ARCH}.deb"
 
 # Ask for input for variables
