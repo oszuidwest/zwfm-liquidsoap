c769a9834d633d22df6091e3dedf9fc8198d5d8b
rmens <r.mens@me.com>
2023-09-16 17:21:59 +0200

    Update icecast2.sh

--- icecast2.sh
+++ icecast2.sh
@@ -1,31 +1,30 @@
 #!/usr/bin/env bash
 
-# Start with a clean terminal
+# Clear terminal
 clear
 
 # Download the functions library
 if ! curl -s -o /tmp/functions.sh https://raw.githubusercontent.com/oszuidwest/bash-functions/main/common-functions.sh; then
-  echo -e  "*** Failed to download functions library. Please check your network connection! ***"
-  exit 1
+    echo -e  "*** Failed to download functions library. Please check your network connection! ***"
+    exit 1
 fi
 
 # Source the functions file
 source /tmp/functions.sh
 
-# Set color variables
+# Configure environment
 set_colors
-
-# Check if we are root
 are_we_root
-
-# Check if this is Linux
 is_this_linux
 is_this_os_64bit
 
+# Print introduction
 clear
 printf "********************************\n"
 printf "ICECAST 2 INSTALLER\n"
 printf "********************************\n"
+
+# Collect user inputs
 ask_user "HOSTNAME" "localhost" "Specify the host name (for example: icecast.zuidwestfm.nl. Enter it without http:// or www) please" "str"
 ask_user "SOURCEPASS" "hackme" "Specify the source and relay password" "str"
 ask_user "ADMINPASS" "hackme" "Specify the admin password" "str"
@@ -34,15 +33,16 @@ ask_user "ADMINMAIL" "root@localhost" "What's the admins e-mail (visible on admi
 ask_user "PORT" "80" "Specify the port" "num"
 ask_user "SSL" "n" "Do you want Let's Encrypt to get a certificate for this server? (y/n)" "y/n"
 
-# If port is not 80 ssl is not possible
+# Check port for SSL possibility
 if [ "$PORT" != "80" ]; then
     SSL="n"
     printf "Since the specified port is not 80, SSL is not possible. Disabling SSL.\n"
 fi
 
-# Set vars
+# Set environment variables
 export DEBIAN_FRONTEND="noninteractive"
 
+# Set debconf selections
 cat << EOF | sudo debconf-set-selections
 icecast2 icecast2/hostname string $HOSTNAME
 icecast2 icecast2/sourcepassword string $SOURCEPASS
@@ -51,13 +51,11 @@ icecast2 icecast2/adminpassword string $ADMINPASS
 icecast2 icecast2/icecast-setup boolean true
 EOF
 
-# Update OS
+# Update and install packages
 update_os silent
-
-# Install packages
 install_packages silent icecast2 certbot
 
-# Post configuration
+# Configure icecast
 sed -i 	-e "s|<location>[^<]*</location>|<location>$LOCATED</location>|" \
 	-e "s|<admin>[^<]*</admin>|<admin>$ADMINMAIL</admin>|" \
 	-e "s|<clients>[^<]*</clients>|<clients>250</clients>|" \
@@ -65,19 +63,20 @@ sed -i 	-e "s|<location>[^<]*</location>|<location>$LOCATED</location>|" \
 	-e "0,/<port>/{s/<port>[0-9]\{1,5\}<\/port>/<port>$PORT<\/port>/;}" \
 	/etc/icecast2/icecast.xml 2>/dev/null 1>&2
 
-# Grant icecast access to ports < 1024
+# Set capabilities
 setcap CAP_NET_BIND_SERVICE=+eip /usr/bin/icecast2
 
-# Apply post configuration
+# Reload and restart services
 systemctl enable icecast2
 systemctl daemon-reload
 service icecast2 restart
 
-# If port is 80 and SSL is enabled, nudge the user to run certbot
+# SSL configuration
 if [ "$PORT" = "80" ] && [ "$SSL" = "y" ]; then
     echo "You should edit icecast.xml to reflect the new port situation and get a certificate with certbot. I can't do that yet..."
 fi
 
+# Commented out section for clarity
 ### SSL IS WIP
 ## This currently doesn't work because let's encrypt _requires_ validation over port 80 while icecast is on port 8000.
 # certbot --quiet --text --agree-tos --email $ADMINMAIL --noninteractive --no-eff-email --webroot --webroot-path="/usr/share/icecast2/web" -d "$HOSTNAME" --deploy-hook "cat /etc/letsencrypt/live/$HOSTNAME/fullchain.pem /etc/letsencrypt/live/$HOSTNAME/privkey.pem > /usr/share/icecast2/icecast.pem && service icecast2 restart" certonly --test-cert --dry-run
