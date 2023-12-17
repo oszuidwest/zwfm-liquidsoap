338c9f51381df95ac469d23c042bb3c130796ab7
rmens <r.mens@me.com>
2023-12-18 00:47:04 +0100

    Formatting

--- install.sh
+++ install.sh
@@ -2,14 +2,16 @@
 
 # Initialize the environment
 clear
+rm -f /tmp/functions.sh
 if ! curl -s -o /tmp/functions.sh https://raw.githubusercontent.com/oszuidwest/bash-functions/main/common-functions.sh; then
-    echo "*** Failed to download functions library. Please check your network connection! ***"
-    exit 1
+  echo "*** Failed to download functions library. Please check your network connection! ***"
+  exit 1
 fi
+
 source /tmp/functions.sh
-set_colors
 
 # Configure environment
+set_colors
 are_we_root
 is_this_linux
 is_this_os_64bit
@@ -24,20 +26,20 @@ OS_ARCH=$(dpkg --print-architecture)
 SUPPORTED_OS=("bookworm" "jammy")
 OS_SUPPORTED=false
 for os in "${SUPPORTED_OS[@]}"; do
-    if [ "$OS_VERSION" == "$os" ]; then
-        OS_SUPPORTED=true
-        break
-    fi
+  if [ "$OS_VERSION" == "$os" ]; then
+    OS_SUPPORTED=true
+    break
+  fi
 done
 if [ "$OS_SUPPORTED" = false ]; then
-    printf "This script does not support '%s' OS version. Exiting.\n" "$OS_VERSION"
-    exit 1
+  printf "This script does not support '%s' OS version. Exiting.\n" "$OS_VERSION"
+  exit 1
 fi
 
 # OS-specific configurations
 if [ "$OS_VERSION" == "bookworm" ]; then
-    cp /etc/apt/sources.list "/etc/apt/sources.list.backup.$(date +%F)"
-    sed -i '/^deb\|^deb-src/ { / non-free \| non-free$/!s/$/ non-free/ }' /etc/apt/sources.list
+  cp /etc/apt/sources.list "/etc/apt/sources.list.backup.$(date +%F)"
+  sed -i '/^deb\|^deb-src/ { / non-free \| non-free$/!s/$/ non-free/ }' /etc/apt/sources.list
 fi
 
 # Set package URLs
@@ -50,7 +52,7 @@ ask_user "USE_ST" "n" "Do you want to use StereoTool for sound processing? (y/n)
 
 # Perform OS updates if desired by user
 if [ "$DO_UPDATES" == "y" ]; then
-    update_os silent
+  update_os silent
 fi
 
 # Install necessary packages
@@ -61,32 +63,34 @@ apt -qq -y install /tmp/liq_2.2.3.deb --fix-broken
 # Create directories and configure them
 dirs=(/etc/liquidsoap /var/audio)
 for dir in "${dirs[@]}"; do
-    mkdir -p "$dir" && \
-    chown liquidsoap:liquidsoap "$dir" && \
-    chmod g+s "$dir"
+  mkdir -p "$dir" && \
+  chown liquidsoap:liquidsoap "$dir" && \
+  chmod g+s "$dir"
 done
 
 # Download and install StereoTool if desired by user
 if [ "$USE_ST" == "y" ]; then
-    install_packages silent unzip
-    mkdir -p /opt/stereotool
-    wget https://download.thimeo.com/Stereo_Tool_Generic_plugin.zip -O /tmp/st.zip
-    unzip -o /tmp/st.zip -d /tmp/
-    EXTRACTED_DIR=$(find /tmp/* -maxdepth 0 -type d -print0 | xargs -0 ls -td | head -n 1)
-    
-    if [ "$OS_ARCH" == "amd64" ]; then
-        cp "${EXTRACTED_DIR}/lib/Linux/IntelAMD/64/libStereoToolX11_intel64.so" /opt/stereotool/st_plugin.so
-        wget https://download.thimeo.com/stereo_tool_cmd_64_1011 -O /opt/stereotool/st_standalone
-    elif [ "$OS_ARCH" == "arm64" ]; then
-        cp "${EXTRACTED_DIR}/lib/Linux/ARM/64/libStereoTool_arm64.so" /opt/stereotool/st_plugin.so
-        wget https://download.thimeo.com/stereo_tool_pi2_64_1011 -O /opt/stereotool/st_standalone
-    fi
-    chmod +x /opt/stereotool/st_standalone
+  install_packages silent unzip
+  mkdir -p /opt/stereotool
+  wget https://download.thimeo.com/Stereo_Tool_Generic_plugin.zip -O /tmp/st.zip
+  unzip -o /tmp/st.zip -d /tmp/
+  EXTRACTED_DIR=$(find /tmp/* -maxdepth 0 -type d -print0 | xargs -0 ls -td | head -n 1)
+  
+  if [ "$OS_ARCH" == "amd64" ]; then
+    cp "${EXTRACTED_DIR}/lib/Linux/IntelAMD/64/libStereoToolX11_intel64.so" /opt/stereotool/st_plugin.so
+    wget https://download.thimeo.com/stereo_tool_cmd_64_1011 -O /opt/stereotool/st_standalone
+  elif [ "$OS_ARCH" == "arm64" ]; then
+    cp "${EXTRACTED_DIR}/lib/Linux/ARM/64/libStereoTool_arm64.so" /opt/stereotool/st_plugin.so
+    wget https://download.thimeo.com/stereo_tool_pi2_64_1011 -O /opt/stereotool/st_standalone
+  fi
+  chmod +x /opt/stereotool/st_standalone
 fi
 
 # Generate and patch StereoTool config file
-/opt/stereotool/st_standalone -X /etc/liquidsoap/st.ini
-sed -i 's/^\(Whitelist=\).*$/\1\/0/' /etc/liquidsoap/st.ini
+if [ "$USE_ST" == "y" ]; then
+  /opt/stereotool/st_standalone -X /etc/liquidsoap/st.ini
+  sed -i 's/^\(Whitelist=\).*$/\1\/0/' /etc/liquidsoap/st.ini
+fi
 
 # Fetch fallback sample and configuration files
 wget https://upload.wikimedia.org/wikipedia/commons/6/66/Aaron_Dunn_-_Sonata_No_1_-_Movement_2.ogg -O /var/audio/fallback.ogg
@@ -97,5 +101,5 @@ rm -f /etc/systemd/system/liquidsoap.service
 wget https://raw.githubusercontent.com/oszuidwest/liquidsoap-ubuntu/main/liquidsoap.service -O /etc/systemd/system/liquidsoap.service
 systemctl daemon-reload
 if ! systemctl is-enabled liquidsoap.service; then
-    systemctl enable liquidsoap.service
+  systemctl enable liquidsoap.service
 fi
