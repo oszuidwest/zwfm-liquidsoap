3cb569bbefed87f4870b08d01c375b159e99ca62
Raymon Mens <r.mens@me.com>
2023-12-18 22:39:40 +0100

    Add SSL to Icecast (#24)

--- icecast2.sh
+++ icecast2.sh
@@ -12,18 +12,27 @@ fi
 # Source the functions file
 source /tmp/functions.sh
 
+# Something fancy for the sysadmin
+cat << "EOF"
+ ______     _     ___          __       _     ______ __  __ 
+|___  /    (_)   | \ \        / /      | |   |  ____|  \/  |
+   / /_   _ _  __| |\ \  /\  / /__  ___| |_  | |__  | \  / |
+  / /| | | | |/ _` | \ \/  \/ / _ \/ __| __| |  __| | |\/| |
+ / /_| |_| | | (_| |  \  /\  /  __/\__ \ |_  | |    | |  | |
+/_____\__,_|_|\__,_|   \/  \/ \___||___/\__| |_|    |_|  |_|
+
+               ********************************
+                      ICECAST 2 INSTALLER
+               ********************************
+			   
+EOF
+
 # Configure environment
 set_colors
 are_we_root
 is_this_linux
 is_this_os_64bit
 set_timezone Europe/Amsterdam
-
-# Print introduction
-clear
-printf "********************************\n"
-printf "ICECAST 2 INSTALLER\n"
-printf "********************************\n"
 
 # Collect user inputs
 ask_user "HOSTNAME" "localhost" "Specify the host name (for example: icecast.zuidwestfm.nl. Enter it without http:// or www) please" "str"
@@ -34,50 +43,91 @@ ask_user "ADMINMAIL" "root@localhost.local" "What's the admins e-mail (visible o
 ask_user "PORT" "80" "Specify the port" "num"
 ask_user "SSL" "n" "Do you want Let's Encrypt to get a certificate for this server? (y/n)" "y/n"
 
-# Check port for SSL possibility
-if [ "$PORT" != "80" ]; then
-    SSL="n"
-    printf "Since the specified port is not 80, SSL is not possible. Disabling SSL.\n"
-fi
-
 # Set environment variables
 export DEBIAN_FRONTEND="noninteractive"
-
-# Set debconf selections
-cat << EOF | sudo debconf-set-selections
-icecast2 icecast2/hostname string $HOSTNAME
-icecast2 icecast2/sourcepassword string $SOURCEPASS
-icecast2 icecast2/relaypassword string $SOURCEPASS
-icecast2 icecast2/adminpassword string $ADMINPASS
-icecast2 icecast2/icecast-setup boolean true
-EOF
 
 # Update and install packages
 update_os silent
 install_packages silent icecast2 certbot
 
-# Configure icecast
-sed -i 	-e "s|<location>[^<]*</location>|<location>$LOCATED</location>|" \
-	-e "s|<admin>[^<]*</admin>|<admin>$ADMINMAIL</admin>|" \
-	-e "s|<clients>[^<]*</clients>|<clients>250</clients>|" \
-	-e "s|<sources>[^<]*</sources>|<sources>5</sources>|" \
-	-e "0,/<port>/{s/<port>[0-9]\{1,5\}<\/port>/<port>$PORT<\/port>/;}" \
-	/etc/icecast2/icecast.xml 2>/dev/null 1>&2
+# Generate initial icecast.xml configuration
+ICECAST_XML="/etc/icecast2/icecast.xml"
+cat <<EOF > "$ICECAST_XML"
+<icecast>
+    <location>$LOCATED</location>
+    <admin>$ADMINMAIL</admin>
+    <hostname>$HOSTNAME</hostname>
+
+    <limits>
+        <clients>1000</clients>
+        <sources>10</sources>
+    </limits>
+
+    <authentication>
+        <source-password>$SOURCEPASS</source-password>
+        <relay-password>$SOURCEPASS</relay-password>
+        <admin-user>admin</admin-user>
+        <admin-password>$ADMINPASS</admin-password>
+    </authentication>
+
+    <listen-socket>
+        <port>$PORT</port>
+    </listen-socket>
+
+    <http-headers>
+        <header name="Access-Control-Allow-Origin" value="*" />
+        <header name="X-Robots-Tag" value="noindex, noarchive" />
+    </http-headers>
+
+    <paths>
+        <basedir>/usr/share/icecast2</basedir>
+        <logdir>/var/log/icecast2</logdir>
+        <webroot>/usr/share/icecast2/web</webroot>
+        <adminroot>/usr/share/icecast2/admin</adminroot>
+        <alias source="/zuidwest.stl" destination="/zuidwest.mp3"/>
+        <alias source="/" destination="/status.xsl"/>
+    </paths>
+
+    <logging>
+        <logsize>10000</logsize>
+    </logging>
+</icecast>
+EOF
 
 # Set capabilities
 setcap CAP_NET_BIND_SERVICE=+eip /usr/bin/icecast2
 
-# Reload and restart services
+# Reload and restart Icecast service
 systemctl enable icecast2
 systemctl daemon-reload
-service icecast2 restart
+systemctl restart icecast2
 
 # SSL configuration
-if [ "$PORT" = "80" ] && [ "$SSL" = "y" ]; then
-    echo "You should edit icecast.xml to reflect the new port situation and get a certificate with certbot. I can't do that yet..."
-fi
+if [ "$SSL" = "y" ] && [ "$PORT" = "80" ]; then
+    # Run Certbot to obtain SSL certificate
+	echo -e "${BLUE}►► Running Certbot to obtain SSL certificate...${NC}"
+    certbot --text --agree-tos --email "$ADMINMAIL" --noninteractive --no-eff-email --webroot --webroot-path="/usr/share/icecast2/web" -d "$HOSTNAME" --deploy-hook "cat /etc/letsencrypt/live/$HOSTNAME/fullchain.pem /etc/letsencrypt/live/$HOSTNAME/privkey.pem > /usr/share/icecast2/icecast.pem && systemctl restart icecast2" certonly
 
-# Commented out section for clarity
-### SSL IS WIP
-## This currently doesn't work because let's encrypt _requires_ validation over port 80 while icecast is on port 8000.
-# certbot --quiet --text --agree-tos --email $ADMINMAIL --noninteractive --no-eff-email --webroot --webroot-path="/usr/share/icecast2/web" -d "$HOSTNAME" --deploy-hook "cat /etc/letsencrypt/live/$HOSTNAME/fullchain.pem /etc/letsencrypt/live/$HOSTNAME/privkey.pem > /usr/share/icecast2/icecast.pem && service icecast2 restart" certonly --test-cert --dry-run
+    # Check if Certbot was successful
+    if [ -f "/usr/share/icecast2/icecast.pem" ]; then
+        # Update icecast.xml with SSL settings
+        sed -i "/<paths>/a \
+        \    <ssl-certificate>/usr/share/icecast2/icecast.pem</ssl-certificate>" "$ICECAST_XML"
+        
+        sed -i "/<\/listen-socket>/a \
+        <listen-socket>\n\
+            <port>443</port>\n\
+            <ssl>1</ssl>\n\
+        </listen-socket>" "$ICECAST_XML"
+
+        # Restart Icecast to apply new configuration
+        echo -e "${BLUE}►► Restarting Icecast with SSL support${NC}"
+		systemctl restart icecast2
+    else
+        echo -e "${YELLOW} !! SSL certificate acquisition failed. Icecast will continue running on port 80.${NC}"
+    fi
+else
+    if [ "$SSL" = "y" ]; then
+        echo -e "${YELLOW} !! SSL setup is only possible when Icecast is running on port 80. Skipping SSL configuration.${NC}"
+    fi
+fi
