2ebebc9791290f899f3cfca33e602714a9431ca2
Raymon Mens <r.mens@me.com>
2024-04-08 00:23:57 +0200

    Refactor (#33)
    * Update install.sh
    * Tone down linter
    * Add second input for the studio
    * Rename repo
    * Update README with audio flow example

--- install.sh
+++ install.sh
@@ -1,102 +1,131 @@
 #!/usr/bin/env bash
 
-# Initialize the environment
-clear
-rm -f /tmp/functions.sh
-if ! curl -s -o /tmp/functions.sh https://raw.githubusercontent.com/oszuidwest/bash-functions/main/common-functions.sh; then
-  echo "*** Failed to download functions library. Please check your network connection! ***"
+# Set-up the functions library
+FUNCTIONS_LIB_PATH="/tmp/functions.sh"
+FUNCTIONS_LIB_URL="https://raw.githubusercontent.com/oszuidwest/bash-functions/main/common-functions.sh"
+
+# Set-up Liquidsoap
+LIQUIDSOAP_VERSION="2.2.4"
+LIQUIDSOAP_PACKAGE_BASE_URL="https://github.com/savonet/liquidsoap/releases/download/v$LIQUIDSOAP_VERSION"
+LIQUIDSOAP_CONFIG_URL="https://raw.githubusercontent.com/oszuidwest/liquidsoap-ubuntu/main/radio.liq"
+LIQUIDSOAP_SERVICE_URL="https://raw.githubusercontent.com/oszuidwest/liquidsoap-ubuntu/main/liquidsoap.service"
+AUDIO_FALLBACK_URL="https://upload.wikimedia.org/wikipedia/commons/6/66/Aaron_Dunn_-_Sonata_No_1_-_Movement_2.ogg"
+
+# Set-up StereoTool
+STEREOTOOL_VERSION="1021"
+STEREOTOOL_BASE_URL="https://download.thimeo.com"
+
+# General options
+SUPPORTED_OS=("bookworm" "jammy")
+TIMEZONE="Europe/Amsterdam"
+DIRECTORIES=("/etc/liquidsoap" "/var/audio")
+
+# Remove old functions library and download the latest version
+rm -f "$FUNCTIONS_LIB_PATH"
+if ! curl -sLo  "$FUNCTIONS_LIB_PATH" "$FUNCTIONS_LIB_URL"; then
+  echo -e "*** Failed to download functions library. Please check your network connection! ***"
   exit 1
 fi
 
-source /tmp/functions.sh
+# Source the functions file
+# shellcheck source=/tmp/functions.sh
+source "$FUNCTIONS_LIB_PATH"
 
-# Configure environment
+# Basic environment configuration
 set_colors
 are_we_root
 is_this_linux
 is_this_os_64bit
-set_timezone Europe/Amsterdam
+set_timezone "$TIMEZONE"
 
-# Detect OS details
-OS_ID=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
-OS_VERSION=$(lsb_release -cs)
-OS_ARCH=$(dpkg --print-architecture)
+# Detect and validate the operating system
+os_id=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
+os_version=$(lsb_release -cs)
+os_arch=$(dpkg --print-architecture)
 
-# Validate OS version
-SUPPORTED_OS=("bookworm" "jammy")
-if [[ ! " ${SUPPORTED_OS[*]} " =~ ${OS_VERSION} ]]; then
-  printf "This script does not support '%s' OS version. Exiting.\n" "$OS_VERSION"
+if [[ ! " ${SUPPORTED_OS[*]} " =~ ${os_version} ]]; then
+  printf "This script does not support '%s' OS version. Exiting.\n" "$os_version"
   exit 1
 fi
 
-# OS-specific configurations
-if [ "$OS_VERSION" == "bookworm" ]; then
+# Start with a clean terminal
+clear
+
+# Banner
+cat << "EOF"
+ ______     _     ___          __       _     ______ __  __ 
+|___  /    (_)   | \ \        / /      | |   |  ____|  \/  |
+   / /_   _ _  __| |\ \  /\  / /__  ___| |_  | |__  | \  / |
+  / /| | | | |/ _` | \ \/  \/ / _ \/ __| __| |  __| | |\/| |
+ / /_| |_| | | (_| |  \  /\  /  __/\__ \ |_  | |    | |  | |
+/_____\__,_|_|\__,_|   \/  \/ \___||___/\__| |_|    |_|  |_|
+EOF
+
+# Greeting
+echo -e "${GREEN}⎎ Liquidsoap and StereoTool setup${NC}\n\n"
+ask_user "USE_ST" "n" "Do you want to use StereoTool for sound processing? (y/n)" "y/n"
+ask_user "DO_UPDATES" "y" "Do you want to perform all OS updates? (y/n)" "y/n"
+
+# OS-specific configurations for Debian Bookworm
+if [ "$os_version" == "bookworm" ]; then
   install_packages silent software-properties-common
   apt-add-repository -y non-free
 fi
 
-# Set package URLs
-BASE_URL="https://github.com/savonet/liquidsoap/releases/download/v2.2.4/liquidsoap_2.2.4"
-PACKAGE_URL="${BASE_URL}-${OS_ID}-${OS_VERSION}-2_${OS_ARCH}.deb"
-
-# User input for script execution
-ask_user "DO_UPDATES" "y" "Do you want to perform all OS updates? (y/n)" "y/n"
-ask_user "USE_ST" "n" "Do you want to use StereoTool for sound processing? (y/n)" "y/n"
-
-# Perform OS updates if desired by user
+# Update OS
 if [ "$DO_UPDATES" == "y" ]; then
   update_os silent
 fi
 
-# Install necessary packages
+# Liquidsoap installation
 install_packages silent fdkaac libfdkaac-ocaml libfdkaac-ocaml-dynlink
-wget "$PACKAGE_URL" -O /tmp/liq_2.2.4.deb
-apt -qq -y install /tmp/liq_2.2.4.deb --fix-broken
+echo -e "${BLUE}►► Installing Liquidsoap...${NC}"
+package_url="${LIQUIDSOAP_PACKAGE_BASE_URL}/liquidsoap_${LIQUIDSOAP_VERSION}-${os_id}-${os_version}-2_${os_arch}.deb"
+liquidsoap_package="/tmp/liquidsoap_${LIQUIDSOAP_VERSION}.deb"
+curl -sLo  "$liquidsoap_package" "$package_url"
+apt -qq -y install "$liquidsoap_package" --fix-broken
 
-# Create directories and configure them
-dirs=(/etc/liquidsoap /var/audio)
-for dir in "${dirs[@]}"; do
-  mkdir -p "$dir" && \
-  chown liquidsoap:liquidsoap "$dir" && \
-  chmod g+s "$dir"
+# Directory setup
+echo -e "${BLUE}►► Creating directories...${NC}"
+for dir in "${DIRECTORIES[@]}"; do
+  mkdir -p "$dir" && chown liquidsoap:liquidsoap "$dir" && chmod g+s "$dir"
 done
 
-# Download and install StereoTool if desired by user
+# Download configuration and sample files
+echo -e "${BLUE}►► Downloading files...${NC}"
+curl -sLo  /var/audio/fallback.ogg "$AUDIO_FALLBACK_URL"
+curl -sLo  /etc/liquidsoap/radio.liq "$LIQUIDSOAP_CONFIG_URL"
+
+# StereoTool setup
 if [ "$USE_ST" == "y" ]; then
   install_packages silent unzip
+  echo -e "${BLUE}►► Installing StereoTool...${NC}"
   mkdir -p /opt/stereotool
-  wget https://download.thimeo.com/Stereo_Tool_Generic_plugin_1021.zip -O /tmp/st.zip
+  curl -sLo  /tmp/st.zip "${STEREOTOOL_BASE_URL}/Stereo_Tool_Generic_plugin_${STEREOTOOL_VERSION}.zip"
   unzip -o /tmp/st.zip -d /tmp/
-  EXTRACTED_DIR=$(find /tmp/* -maxdepth 0 -type d -print0 | xargs -0 ls -td | head -n 1)
+  extracted_dir=$(find /tmp/* -maxdepth 0 -type d -print0 | xargs -0 ls -td | head -n 1)
   
-  if [ "$OS_ARCH" == "amd64" ]; then
-    cp "${EXTRACTED_DIR}/lib/Linux/IntelAMD/64/libStereoToolX11_intel64.so" /opt/stereotool/st_plugin.so
-    wget https://download.thimeo.com/stereo_tool_cmd_64_1021 -O /opt/stereotool/st_standalone
-  elif [ "$OS_ARCH" == "arm64" ]; then
-    cp "${EXTRACTED_DIR}/lib/Linux/ARM/64/libStereoTool_arm64.so" /opt/stereotool/st_plugin.so
-    wget https://download.thimeo.com/stereo_tool_pi2_64_1021 -O /opt/stereotool/st_standalone
+  if [ "$os_arch" == "amd64" ]; then
+    cp "${extracted_dir}/lib/Linux/IntelAMD/64/libStereoToolX11_intel64.so" /opt/stereotool/st_plugin.so
+    curl -sLo  /opt/stereotool/st_standalone "${STEREOTOOL_BASE_URL}/stereo_tool_cmd_64_${STEREOTOOL_VERSION}"
+  elif [ "$os_arch" == "arm64" ]; then
+    cp "${extracted_dir}/lib/Linux/ARM/64/libStereoTool_arm64.so" /opt/stereotool/st_plugin.so
+    curl -sLo  /opt/stereotool/st_standalone "${STEREOTOOL_BASE_URL}/stereo_tool_pi2_64_${STEREOTOOL_VERSION}"
   fi
   chmod +x /opt/stereotool/st_standalone
-fi
 
-# Generate and patch StereoTool config file
-if [ "$USE_ST" == "y" ]; then
+  # Generate and patch StereoTool config file
   /opt/stereotool/st_standalone -X /etc/liquidsoap/st.ini
   sed -i 's/^\(Whitelist=\).*$/\1\/0/' /etc/liquidsoap/st.ini
+else
+  # If StereoTool is not used, remove its configuration from the liquidsoap script
+  sed -i '/# StereoTool implementation/,/output.dummy(radioproc)/d' /etc/liquidsoap/radio.liq
 fi
 
-# Fetch fallback sample and configuration files
-wget https://upload.wikimedia.org/wikipedia/commons/6/66/Aaron_Dunn_-_Sonata_No_1_-_Movement_2.ogg -O /var/audio/fallback.ogg
-wget https://raw.githubusercontent.com/oszuidwest/liquidsoap-ubuntu/main/radio.liq -O /etc/liquidsoap/radio.liq
-
-# Comment out the StereoTool implementation if not enabled
-if [ "$USE_ST" == "y" ]; then
-  sed -i '/# StereoTool implementation/,/output.dummy(radioproc)/ s/^#//' "/etc/liquidsoap/radio.liq"
-fi
-
-# Install and set up service
+# Liquidsoap service installation
+echo -e "${BLUE}►► Setting up Liquidsoap service${NC}"
 rm -f /etc/systemd/system/liquidsoap.service
-wget https://raw.githubusercontent.com/oszuidwest/liquidsoap-ubuntu/main/liquidsoap.service -O /etc/systemd/system/liquidsoap.service
+curl -sLo  /etc/systemd/system/liquidsoap.service "$LIQUIDSOAP_SERVICE_URL"
 systemctl daemon-reload
 if ! systemctl is-enabled liquidsoap.service; then
   systemctl enable liquidsoap.service
